df8ddf2d9895efd29995ad9176afe50f1220422f
IvanFedyanin <fivan78@yandex.ru>
2025-08-05 22:52:02 +0400

    урок "Stream API часть 3"

--- src/ru/fivan/StreamAPI_Test/StreamClassExsample3.java
+++ src/ru/fivan/StreamAPI_Test/StreamClassExsample3.java
@@ -1,18 +1,30 @@
 package ru.fivan.StreamAPI_Test;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamClassExsample3 {
     public static void main(String[] args) {
         Map<String,Integer> coins = new HashMap<String, Integer>(){{
-            put("CAD",235);
+            put("CAD",23);
             put("LRD",25);
             put("NZD",35);
             put("SEK",230);
         }};
 
         System.out.println(coins);
+
+        Map<String,Integer> reverse = coins.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,
+                        (first, second) ->first, LinkedHashMap::new));
+
+        System.out.println(reverse);
+
     }
+
 
 }
